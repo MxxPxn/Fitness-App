357f99f109e005b6a798ac591dcd7011ce5d86e2
Max <max3panri@outlook.com>
2025-05-06 08:20:57 -0400

    add GalleryPage import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"; 
 import './styles/nulling.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import TopTitle from "./components/TopTitle/TopTitle";
@@ -9,19 +10,30 @@ import Plans from "./components/Plans/Plans";
 import TrainingSteps from "./components/TrainingSteps/TrainingSteps";
 import Map from "./components/Map/Map";
 import Footer from "./components/Footer/Footer";
+import Gallery from "./pages/GalleryPage";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <TopTitle />
-            <Support />
-            <Category />
-            <Plans />
-            <TrainingSteps />
-            <Map />
-            <Footer />
-        </div>
-    )
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={
+                   <>
+                    <TopTitle />
+                    <Support />
+                    <Category />
+                    <Plans />
+                    <TrainingSteps />
+                    <Map />
+                    <Footer />
+                    
+                    </>
+                } />
+                <Route path="/gallery" element={<Gallery />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 export default App;
