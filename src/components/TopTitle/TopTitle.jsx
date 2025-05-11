ffb08ad68fbe6aaa80d48dfa876f7e64b8522572
Max <max3panri@outlook.com>
2025-05-10 22:04:56 -0400

    change input type to email

--- src/components/TopTitle/TopTitle.jsx
+++ src/components/TopTitle/TopTitle.jsx
@@ -7,7 +7,7 @@ export const TopTitle = () => {
             <div className="title"><h2>Shape your body with us here</h2></div>
             <div className="subtext">Join our fitness community and start your journey to a healthier lifestyle. Our expert trainers and customized programs will help you achieve your fitness goals.</div>
             <div className="inputform">
-                <input type="text" placeholder="Enter your email here" />
+                <input type="email" placeholder="Enter your email here" />
                 <button className="active-but">Get Started</button>
             </div>
         </div>
