cd3dc8a7db41b3912faa93622b43065a91e59214
Max <max3panri@outlook.com>
2025-05-20 21:56:54 -0400

    apdate footer-links add classes change buttons for a tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,59 +4,81 @@ import logoIcon from '../../assets/icons/logo.svg';
 import './Footer.scss';
 
 export const Footer = () => {
-    return(
+    return (
         <div className="footer">
             <div className="footer-content">
-            <div className="footer-logo">
-                <img src={logoIcon} alt="logo" />
-                <p>Join MaxFit today and start your journey to a healthier, 
-                stronger you. Expert trainers, state-of-the-art facilities, and personalized programs await!</p>
-            </div>
+                <div className="footer-logo">
+                    <img src={logoIcon} alt="logo" />
+                    <p>
+                        Join MaxFit today and start your journey to a healthier,
+                        stronger you. Expert trainers, state-of-the-art facilities, and personalized programs await!
+                    </p>
+                </div>
                 <div className="footer-links">
-                    <h4>Info</h4>
-                    <ul>
-                        <li><button onClick={() => {}}>Newsroom</button></li>
-                        <li><button onClick={() => {}}>FAQs</button></li>
-                        <li><button onClick={() => {}}>Site Map</button></li>
-                        <li><button onClick={() => {}}>Careers</button></li>
+                    <h3 className="footer-title"> Info </h3>
+                    <ul className="footer-list">
+                        <li className="footer-item">
+                            <a href="#">Newsroom</a>
+                        </li>
+
+                        <li className="footer-item">
+                            <a href="#">FAQs</a>
+                        </li>
+
+                        <li className="footer-item">
+                            <a href="#">Site Map</a>
+                        </li>
+
+                        <li className="footer-item">
+                            <a href="#">Careers</a>
+                        </li>
+
                     </ul>
                 </div>
-    
+
                 <div className="footer-links">
-                    <h4>Partners</h4>
-                    <ul>
-                        <li><button onClick={() => {}}>Franchising</button></li>
-                        <li><button onClick={() => {}}>Investor Relations</button></li>
-                        <li><button onClick={() => {}}>PF Purpose</button></li>
+                    <h3 className="footer-title">Partners</h3>
+                    <ul className="footer-list">
+                        <li className="footer-item">
+                            <a href="#">Franchising</a>
+                        </li>
+                        <li className="footer-item">
+                            <a href="#">Investor Relations</a>
+                        </li>
+                        <li className="footer-item">
+                            <a href="#">PF Purpose</a>
+                        </li>
                     </ul>
                 </div>
-    
+
                 <div className="footer-links">
-                    <h4>Legal</h4>
-                    <ul>
-                        <li><button onClick={() => {}}>Privacy Policy</button></li>
-                        <li><button onClick={() => {}}>Terms and Conditions</button></li>
-                        <li>
-                        <button onClick={() => {}}>Share Information</button>
-                    </li>
-                        <li><button onClick={() => {}}>Accessibility</button></li>
+                    <h3 className="footer-title">Legal</h3>
+                    <ul className="footer-list">
+                        <li className="footer-item">
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li className="footer-item">
+                            <a href="#">Terms and Conditions</a>
+                        </li>
+                        <li className="footer-item">
+                            <a href="#">Share Information</a>
+                        </li>
+                        <li className="footer-item">
+                            <a href="#">Accessibility</a>
+                        </li>
                     </ul>
-                </div>
-    
-                <div className="Region-language">
-    
                 </div>
                 
             </div>
-        <div className="footer-bottom">
-                    <p>©2024 MaxFit</p>
-                    <div className="social-media">
-                        <button onClick={() => {}}> X (Twitter)</button>
-                        <button onClick={() => {}}>Instagram</button>
-                        <button onClick={() => {}}>Facebook</button>
-                    </div>
+            <div className="footer-bottom">
+                <p>©2024 MaxFit</p>
+                <div className="social-media">
+                    <a href="#">X (Twitter)</a>
+                    <a href="#">Instagram</a>
+                    <a href="#">Facebook</a>
                 </div>
+            </div>
         </div>
-    )
+    );
 }
 export default Footer;
